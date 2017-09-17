b518d3df6d2e3b096ef56e860d27567e92f6c2d6
Ivan Degtyarov <sevuha26@gmail.com>
2017-09-17 04:06:25 +0300

    Monitoring&Resursi

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
+    <script type="text/javascript" src="js/jquery.min.js"></script>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -16,7 +16,10 @@
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body>
+<?}
+?>
+ </body>
+  </form>
     <nav class="navbar navbar-default" role="navigation">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -33,8 +36,68 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">ТВ</a></li>
-        <li><a href="#">Коммутаторы</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Ресурсы ЗТ<b class="caret"></b></a>
+          <ul class="dropdown-menu">
+           <li role="presentation" class="dropdown-header">Ставрополь</li>
+            <li><a href="https://erp.ooonet.ru" rel="external"><script type="text/javascript">$('a[rel=external]').attr('target','_blank');</script>ERPCore</a></li>
+            <li><a href="https://stavropol.groupw.ru" rel="external"><script type="text/javascript">$('a[rel=external]').attr('target','_blank');</script>GW</a></li>
+	    <li><a href="https://us.ooonet.ru" rel="external"><script type="text/javascript">$('a[rel=external]').attr('target','_blank');</script>UserSide</a></li>
+            <li class="divider"></li>
+	    <li role="presentation" class="dropdown-header">Невиномыск</li>
+            <li><a href="https://erp.ooonet.ru/userside-nev/oper/admin.php" rel="external"><script type="text/javascript">$('a[rel=external]').attr('target','_blank');</script>UserSide</a></li>
+            <li><a href="https://noc.ooonet.ru/report-gss/switch_status/" rel="external"><script type="text/javascript">$('a[rel=external]').attr('target','_blank');</script>switch_status</a></li>
+            <li><a href="https://noc.ooonet.ru/report-gss/user_status/" rel="external"><script type="text/javascript">$('a[rel=external]').attr('target','_blank');</script>user_status</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Мониторинг<b class="caret"></b></a>
+          <ul class="dropdown-menu">
+           <li role="presentation" class="dropdown-header">Ставрополь</li>
+           <li><a href="https://zabbix.ooonet.ru" rel="external"><script type="text/javascript">$('a[rel=external]').attr('target','_blank');</script>Zabbix</a></li>
+           <li><a href="https://nagios.ooonet.ru" rel="external"><script type="text/javascript">$('a[rel=external]').attr('target','_blank');</script>Nagios</a></li>
+	   <li><a href="http://mon.main.zelenaya.net" rel="external"><script type="text/javascript">$('a[rel=external]').attr('target','_blank');</script>IPTV</a></li>
+           <li class="divider"></li>
+	   <li role="presentation" class="dropdown-header">Формула связи</li>
+           <li><a href="http://nagios.iformula.ru">Nagios</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Содержание <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li class="divider"></li>
+            <li><a href="#">4</a></li>
+            <li class="divider"></li>
+            <li><a href="#">5</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Содержание <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li class="divider"></li>
+            <li><a href="#">4</a></li>
+            <li class="divider"></li>
+            <li><a href="#">5</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Содержание <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li class="divider"></li>
+            <li><a href="#">4</a></li>
+            <li class="divider"></li>
+            <li><a href="#">5</a></li>
+          </ul>
+        </li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Содержание <b class="caret"></b></a>
           <ul class="dropdown-menu">
@@ -48,33 +111,33 @@
           </ul>
         </li>
       </ul>
-      <form class="navbar-form navbar-left" role="search">
+      <form class="navbar-form navbar-right" role="search">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Search">
         </div>
-        <button type="submit" class="btn btn-default">Отправить</button>
+        <button type="submit" class="btn btn-default">Найти</button>
       </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Ссылка</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">ТВ Порталы<b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li><a href="https://admin.zelenaya.iptvportal.ru/">Ставрополь Home ip</a></li>
-            <li><a href="#">Невинка Сталкер</a></li>
-            <li><a href="http://admin.nevinka.iptvportal.ru/">Невинка Home ip</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Руководство по порталам</a></li>
-          </ul>
-        </li>
-      </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+ <script $(function(){
+    $('.dropdown').on('show.bs.dropdown', function(e){
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    });
+    $('.dropdown').on('hide.bs.dropdown', function(e){
+        e.preventDefault();
+	var self = this;
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp(400, function(){
+            $(self).removeClass('open');
+            $(self).find('.dropdown-toggle').attr('aria-expanded','false');
+        });
+    });
+});></script>
   </body>
 </html>
 
